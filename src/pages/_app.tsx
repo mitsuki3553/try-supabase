2942719e53a5a3227cbf762d877ab0e06d07ccbd
mitsuki <mitsuki3553@gmail.com>
2022-01-06 14:37:36 +0900

    ⚡️ test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
-
+//aaaaaaa
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
