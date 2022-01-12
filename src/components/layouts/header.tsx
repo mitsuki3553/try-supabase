d86cf94e1b9dda2743f55baf32d5fe1baba1e5c3
mitsuki <mitsuki3553@gmail.com>
2022-01-13 07:02:42 +0900

    🎨 記述変更

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="flex justify-center gap-4 py-6 text-gray-600 bg› ›-gray-200">
+    <header className="flex justify-center gap-4 py-6 text-gray-600 bg-gray-200">
       {/* <Link href="/">
         <a><Image src={logo} alt="logo" width={75} height={75} /></a>
       </Link> */}
       <Link href="/">
         <a className="text-4xl text-center">
-          <h1 className="pt-2 m-2">Manga List</h1>
+          <h1 className="pt-2 m-2">つぶやくやつ(仮)</h1>
         </a>
       </Link>
     </header>
