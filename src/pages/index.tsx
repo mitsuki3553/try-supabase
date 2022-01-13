0fd56834c6f8567fb1802a4e6d4ec1acf4d4d5d2
mitsuki <mitsuki3553@gmail.com>
2022-01-13 20:50:42 +0900

    👔 indexページのロジック実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import { ReactNode, useEffect, useState } from "react";
-
-import { Auth, Button, IconLogOut } from "@supabase/ui";
-
+import { ReactNode, useState } from "react";
+import { Button, IconLogOut } from "@supabase/ui";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { supabase } from "src/libs/supabase";
-import { LayoutWrapper } from "src/components/layouts/layoutWrapper";
 import { InputProfile } from "src/components/modal_contents/input_profile";
-import { useForm, UseFormRegisterReturn } from "react-hook-form";
-import toast from "react-hot-toast";
+import { getPosts, getProfile, postPost } from "src/components/functions/supabase";
 
 type Props = {
   children: ReactNode;
@@ -19,56 +18,30 @@ type Profile = {
   username: string;
 };
 
-type Post={
-  user_id:string;
-  post_id:string;
-  posts:string;
-  created_at:Date;
-  updated_at:Date;
-  public:boolean;
-}
-
-//ユーザー情報をGETする
-const getProfile = async () => {
-  const { data, error } = await supabase.from("profiles").select("*");
-
-  if (!error && data) {
-    return data;
-  }
-  return null;
+type Post = {
+  user_id: string;
+  post_id: string;
+  posts: string;
+  created_at: Date;
+  updated_at: Date;
+  public: boolean;
 };
-//投稿をPOSTする
-const postPost = async (post:string,uuid:string)=>{
-  const { error } = await supabase
-  .from("post_table")
-  .insert([{ user_id: uuid , posts: post ,created_at: new Date(),updated_at: new Date(),public:true}]);
-    if (error) toast.error(error.message);
-}
-//投稿をGETする
-const getPosts = async ()=>{
-  const { data, error } = await supabase.from("post_table").select("*");
-console.log(data);
 
-  if (!error && data) {
-    return data as Post[];
-  }
-  return null;
-}
+export default function Home() {
+  const session = supabase.auth.session();
+  const { replace } = useRouter();
+  const { register, handleSubmit } = useForm({
+    mode: "onSubmit",
+    reValidateMode: "onChange",
+  });
 
-const Container = () => {
-  const { user } = Auth.useUser();
-  const [profile, setProfile] = useState<Profile | null>(null);
-  const [posts, setPosts] = useState<Post[] | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-    const { register, handleSubmit } = useForm({
-      mode: "onSubmit",
-      reValidateMode: "onChange",
-      criteriaMode: "firstError",
-    });
+    const [profile, setProfile] = useState<Profile | null>(null);
+    const [posts, setPosts] = useState<Post[] | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    if(user){
-      const fetchData = async()=>{
+    if (session) {
+      const fetchData = async () => {
         const fetchedProfile = await getProfile();
         const posts = await getPosts();
         setPosts(posts);
@@ -76,62 +49,54 @@ const Container = () => {
         setIsLoading(false);
       };
       fetchData();
+    } else {
+      //localstrageにログイン情報がなければログイン画面へ
+      replace("/signin");
     }
-  }, [user]);
-  
-  // ログインしている場合
-  if (user) {
-    return (
-      <>
-        <form onSubmit={handleSubmit((data) => postPost(data.post,user.id))}>
+  }, [session]);
+
+  return (
+    <div className="h-screen flex center items-center justify-center">
+      <div className="sm:max-w-xl bg-white  w-full sm:rounded-lg p-5 shadow">
+        <h2>ログイン中</h2>
+        <form
+          className="mt-2"
+          onSubmit={handleSubmit((data) => postPost(data.post, session?.user?.id!))}
+        >
           <textarea
             placeholder="ここで投稿してね！"
-            {...register("post",{required:true})}
+            className="w-full"
+            {...register("post", { required: true })}
           />
-          <button type="submit">投稿！</button>
+          <Button role="submit" block>
+            投稿！
+          </Button>
         </form>
         <div className="flex flex-col">
-          {posts?.map((post)=>{
+          {posts?.map((post) => {
             return (
               <div key={post.post_id} className="border">
                 <p>{post.created_at}</p>
                 <p>{post.posts}</p>
+                <form>
+                  <Button role="submit">コメント</Button>
+                </form>
               </div>
             );
           })}
         </div>
+        {!isLoading && !profile && <InputProfile uuid={session?.user?.id!} />}
+
         <Button
-          size="medium"
-          icon={<IconLogOut />}
-          onClick={() => supabase.auth.signOut()}
+          block
+          onClick={() => {
+            supabase.auth.signOut()
+            replace("/");
+          }}
         >
-          Sign out
+          サインアウト
         </Button>
-        {!isLoading && !profile && <InputProfile uuid={user.id} />}
-      </>
-    );
-  }
-  // ログインしていない場合
-  return (
-    <div className="flex justify-center pt-8">
-      <div className="w-full sm:w-96">
-        <Auth
-          supabaseClient={supabase}
-          providers={["github"]}
-          socialColors={true}
-        />
       </div>
     </div>
   );
-};
-
-const Home = () => {
-  return (
-    <LayoutWrapper>
-      <Auth.UserContextProvider supabaseClient={supabase}>
-        <Container />
-      </Auth.UserContextProvider>
-    </LayoutWrapper>
-  );
-};
-export default Home;
+}
