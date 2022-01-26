5dc73eab5cdae736c4d7f6844e710a6401a414b7
mitsuki <mitsuki3553@gmail.com>
2022-01-26 13:21:02 +0900

    🎨 ボタンの位置修正

--- src/components/molecules/layouts/header.tsx
+++ src/components/molecules/layouts/header.tsx
@@ -6,26 +6,30 @@ import { supabase } from "src/libs/supabase";
 // import logo from "public/logo.png";
 
 export const Header = () => {
+  const session = supabase.auth.session();
+  
   const { replace } = useRouter();
   return (
-    <header className=" gap-4 py-2 text-gray-600 bg-gray-200">
+    <header className=" gap-4 py-2 text-gray-600 bg-gray-200 grid grid-cols-3">
       {/* <Link href="/">
         <a><Image src={logo} alt="logo" width={75} height={75} /></a>
       </Link> */}
+      <div className="col-start-2">
       <Link href="/">
-        <a className="text-4xl text-center">
+        <a className="text-4xl text-center ">
           <h1 className="pt-2 m-2">Post Next</h1>
         </a>
       </Link>
-      <span className="block text-right px-4">
-        <Button
+      </div>
+      <span className="block text-right px-4 my-auto">
+        {session && <Button
           onClick={() => {
             supabase.auth.signOut();
             replace("/");
           }}
         >
           サインアウト
-        </Button>
+        </Button>}
       </span>
     </header>
   );
